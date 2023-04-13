4e447763c5d7290ecddd8d2b840408021f63f51f
MaximilianLeyh <118059860+MaximilianLeyh@users.noreply.github.com>
2023-04-13 15:25:15 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,66 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { FormsModule } from '@angular/forms';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatCardModule} from '@angular/material/card';
+import {MatMenuModule} from '@angular/material/menu';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { HomeComponent } from './home/home.component';
+import { PostOverviewComponent } from './post-overview/post-overview.component';
+import { ThreadsComponent } from './threads/threads.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent ,
+      HomeComponent,
+      PostOverviewComponent,
+      ThreadsComponent
+    ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatButtonToggleModule,
+    MatDialogModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    FormsModule,
+    MatCardModule,
+    MatMenuModule,
+    MatProgressBarModule,
+    AngularFirestoreModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyBwldrRthGD4-FOjZMXIZGJirxlyUmekpA",
+        authDomain: "slack-clone-5176c.firebaseapp.com",
+        projectId: "slack-clone-5176c",
+        storageBucket: "slack-clone-5176c.appspot.com",
+        messagingSenderId: "188328797857",
+        appId: "1:188328797857:web:c65aeda2c2ed7930c432e0"
+    },
+    production: true
+  };
